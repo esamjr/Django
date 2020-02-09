ca53bb3e0dc317ba8906924870da00a934d82d36
Agil Dimas <agildimasjr@gmail.com>
2020-02-09 17:22:00 +0700

    Nampilin data dari db

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,19 +1,15 @@
 # Views blog
 
 from django.shortcuts import render
-
-# Variable
-context={
-    'judul':'Blog Page',
-    'nav': [
-        ['/','Home'],
-        ['/blog/cerita','Cerita Blog'],
-        ['/blog/news','News'],
-        ['/about','About']
-    ]
-}
+from .models import Post
 # Create your views here.
-
 def index(request):
+    #queryset 
+    posts = Post.objects.all()
+    context={
+    'judul':'Blog Page',
+    'subjudul':'Selamat Datang Di Blog Django',
+    'Posts':posts,
+}
     return render(request,'blog/index.html', context)
 
